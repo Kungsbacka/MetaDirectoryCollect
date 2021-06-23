6eea427c84544ab85b1e8d1007181c92a126b9b7
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2021-06-23 21:12:24 +0200

    Expand alias

--- git-hooks/pre-commit.ps1
+++ git-hooks/pre-commit.ps1
@@ -4,7 +4,7 @@ Get-ChildItem  "$PSScriptRoot\..\..\*.dtsx" -Recurse | ForEach-Object {
     if ($package -like '*\bin\Development\*' -or $package -like '*\obj\Development\*') {
         return # bin and obj folders are not included in the repository and can be excluded from this check
     }
-    ([xml](gc $_.FullName)).Executable.PackageParameters.PackageParameter | ForEach-Object {
+    ([xml](Get-Content $_.FullName)).Executable.PackageParameters.PackageParameter | ForEach-Object {
         if ($_.DataType -eq '8' -and $_.Property.'#text'.Length -gt 0) {
             $nonEmpty += [pscustomobject]@{
                 Parameter = $_.ObjectName
@@ -18,7 +18,7 @@ Get-ChildItem  "$PSScriptRoot\..\..\*.params" -Recurse | ForEach-Object {
     if ($package -like '*\bin\Development\*' -or $package -like '*\obj\Development\*') {
         return # bin and obj folders are not included in the repository and can be excluded from this check
     }
-    ([xml](gc $_.FullName)).Parameters.Parameter | ForEach-Object {
+    ([xml](Get-Content $_.FullName)).Parameters.Parameter | ForEach-Object {
         $dataType = $_.Properties.Property | Where-Object Name -EQ 'DataType' | Foreach-Object '#text'
         $value = $_.Properties.Property | Where-Object Name -EQ 'Value' | Foreach-Object '#text'
         if ($dataType -eq '18' -and $value.Length -gt 0) {
